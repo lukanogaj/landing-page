3501bcf967bfd063a327fc5baf674a7cc883c157
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-04-20 14:15:36 +0100

    section-one

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,14 @@
 import styles from "./index.module.scss";
 import Navbar from "../NavBar";
 import HeroContent from "../HeroContent";
+import SectionOne from "../SectionOne";
+
 const Hero = () => {
 	return (
 		<div className={styles.hero}>
 			<Navbar />
 			<HeroContent />
+			<SectionOne />
 		</div>
 	);
 };
---
+++ src/components/SectionOne/index.jsx
@@ -0,0 +1,49 @@
+import styles from "./index.module.scss";
+
+const SectionOne = () => {
+	return (
+		<div className={styles.sectionOne}>
+			<div className={styles.sectionOneText}>
+				<div className={styles.sectioOneHeading}>
+					<h1>Light, Fast & PowerFull</h1>
+				</div>
+				<div className={styles.sectionOneParagraph}>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
+					culpa hic dicta maiores architecto molestiae laborum, iste quas ullam
+					illo rem <br />
+					<br />
+					cum laboriosam nesciunt molestias velit quidem sint! Laudantium
+					voluptate atque, mollitia deleniti quis libero nostrum aperiam nam eos
+					saepe?
+				</div>
+			</div>
+			<div className={styles.icons}>
+				<div className={styles.titleContent}>
+					<div className={styles.icon}></div>
+					<h2>Title Goes Here</h2>
+					<div className={styles.iconsParagraph}>
+						<p>
+							Lorem ipsum dolor sit amet consectetur <br />
+							adipisicing elit. Rerum officiis quis nesciunt? <br /> Atque!
+						</p>
+					</div>
+				</div>
+				<div>
+					<div className={styles.titleContent}>
+						<div className={styles.icon}></div>
+						<h2>Title Goes Here</h2>
+						<div className={styles.iconsParagraph}>
+							<p>
+								Lorem, ipsum dolor sit amet consectetur <br /> adipisicing elit.
+								Saepe amet recusandae ducimus
+								<br /> voluptatem.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SectionOne;
