24cd5863d2f79156de1edef51de33c157026f79e
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-04-22 12:55:09 +0100

    height-set

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -5,8 +5,10 @@ const Intro = () => {
 	return (
 		<div className={styles.intro}>
 			{dataForIntro.map((card) => (
-				<div className={styles.introText}>
-					<div className={styles.introIcon}>{card.image}</div>
+				<div className={styles.introText} key={card.id}>
+					<div className={styles.introIcon}>
+						<img src={card.image} alt="" />
+					</div>
 					<div className={styles.introContent}>
 						<div className={styles.introHeading}>
 							<h1>Light, Fast & PowerFull</h1>
@@ -29,36 +31,3 @@ const Intro = () => {
 };
 
 export default Intro;
-
-{
-	/* <div className={styles.introText}>
-				<div className={styles.introIcon}></div>
-				<div className={styles.introContent}>
-					<div className={styles.introHeading}>
-						<h1>Light, Fast & PowerFull</h1>
-					</div>
-					<div className={styles.introParagraph}>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
-						culpa hic dicta maiores architecto molestiae laborum, iste quas
-						ullam illo rem
-						<br />
-						<br />
-						cum laboriosam nesciunt molestias velit quidem sint! Laudantium
-						voluptate atque, mollitia deleniti quis libero nostrum aperiam nam
-						eos saepe?
-					</div>
-				</div>
-			</div> */
-}
-{
-	/*  */
-}
-{
-	/*  */
-}
-{
-	/*  */
-}
-{
-	/*  */
-}
