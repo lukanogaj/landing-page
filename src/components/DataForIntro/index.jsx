762491e9312a4ba3e962f54957ca15432cfd29ef
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-04-22 11:31:41 +0100

    data-for-intros

---
+++ src/components/DataForIntro/index.jsx
@@ -0,0 +1,24 @@
+import pictureOne from "../media/Icons/intro1.png";
+import pictureTwo from "../media/Icons/intro2.png";
+import pictureThree from "../media/Icons/intro3.png";
+
+const DataForIntro = () => [
+	{
+		id: 1,
+		image: pictureOne,
+		paragraph:
+			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam nostrum autem enim, repellat expedita, dolorum suscipit quo possimus perferendis accusamus illo, beatae saepe? Repudiandae, ratione nam libero, porro dolores inventore non consequatur consectetur enim vel maiores sequi, quaerat velit.",
+	},
+	{
+		id: 2,
+		image: pictureTwo,
+		paragraph:
+			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam nostrum autem enim, repellat expedita, dolorum suscipit quo possimus perferendis accusamus illo, beatae saepe? Repudiandae, ratione nam libero, porro dolores inventore non consequatur consectetur enim vel maiores sequi, quaerat velit.",
+	},
+	{
+		id: 3,
+		image: pictureThree,
+		paragraph:
+			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam nostrum autem enim, repellat expedita, dolorum suscipit quo possimus perferendis accusamus illo, beatae saepe? Repudiandae, ratione nam libero, porro dolores inventore non consequatur consectetur enim vel maiores sequi, quaerat velit.",
+	},
+];
